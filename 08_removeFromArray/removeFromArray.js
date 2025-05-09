1ea33f700cc390fe18bbb05b3572ecba6f193852
Lauri Forss <lauri.forss@gmail.com>
2025-05-09 16:14:03 +0300

    pseudokoodi kirjoitettu

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,a) {
+    // luo arrayLista muuttuvasta argumenttimäärästä
+
+    // käy läpi arr jokainen kohta
+
+    // etsi onko arr:ssa argumentteja
+
+    // jos on luo niiden indekseistä oma arrLista
+
+    // käy läpi jokainen arr kohta ja poista jos se on arrListassa
 };
 
 // Do not edit below this line
