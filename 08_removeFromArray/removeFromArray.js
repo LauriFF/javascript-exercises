4aceb3fb4c198409a5161e4ec843ef8cdae67d8a
Lauri Forss <lauri.forss@gmail.com>
2025-05-11 13:54:38 +0300

    testit saatu toimivaksi, ongelma oli 'arguments' sanan käyttäminen

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
-const removeFromArray = function(arr, ...arguments) {
+const removeFromArray = function(arr, ...argumens) {
     // luo arrayLista muuttuvasta argumenttimäärästä
     let indexes = [];
     // käy läpi arr jokainen kohta
     // console.log(`arguments: ${arguments}`)
     for (let i = 0; i < arr.length; i++) {
         // etsi onko arr:ssa argumentteja
-        for (let j = 0; j < arguments.length; j++) {
-            if(arr[i] == arguments[j]) {
+        for (let j = 0; j < argumens.length; j++) {
+            if(arr[i] == argumens[j]) {
                 indexes.push(i);
             }
         }
@@ -19,13 +19,12 @@ const removeFromArray = function(arr, ...arguments) {
 
     // käy läpi jokainen arr kohta ja poista jos se on arrListassa
     for (let i = indexes.length -1; i >= 0; i--) {
-        arr.splice(indexes[i],1)
+        arr.splice(indexes[i],1);
     }
     // console.log(arr);
     // jos on luo niiden indekseistä oma arrLista
     return arr;
 };
-// removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
