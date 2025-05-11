240bd9a6a67fc5a315ceff29d7d7f3797cbd217c
Lauri Forss <lauri.forss@gmail.com>
2025-05-11 13:07:17 +0300

    yksin yritetty vastaus, tekoäly opastajana

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,14 +1,31 @@
-const removeFromArray = function(arr,a) {
+const removeFromArray = function(arr, ...arguments) {
     // luo arrayLista muuttuvasta argumenttimäärästä
-
+    let indexes = [];
     // käy läpi arr jokainen kohta
+    // console.log(`arguments: ${arguments}`)
+    for (let i = 0; i < arr.length; i++) {
+        // etsi onko arr:ssa argumentteja
+        for (let j = 0; j < arguments.length; j++) {
+            if(arr[i] == arguments[j]) {
+                indexes.push(i);
+            }
+        }
+    }
+    // console.log(`indexes: ${indexes}`)
 
-    // etsi onko arr:ssa argumentteja
-
-    // jos on luo niiden indekseistä oma arrLista
+    indexes.sort((a,b) => b - a );
+    // console.log(`indexes: ${indexes}`)
+    // console.log(`arr: ${arr}`)
 
     // käy läpi jokainen arr kohta ja poista jos se on arrListassa
+    for (let i = indexes.length -1; i >= 0; i--) {
+        arr.splice(indexes[i],1)
+    }
+    // console.log(arr);
+    // jos on luo niiden indekseistä oma arrLista
+    return arr;
 };
+// removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
