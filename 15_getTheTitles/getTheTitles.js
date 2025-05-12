1b1930918b495420048c1c40ebb4e809439bc5b3
Lauri Forss <lauri.forss@gmail.com>
2025-05-12 15:25:49 +0300

    obj harjoittelu

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // käy läpi objektin jokainen kohta ja poimi titlet ja luo niistä array
+    const titlet = books.reduce((acc, currentValue) => {
+        acc.push(currentValue.title);
+        return acc;
+    },[]);
+    // console.log(titlet);
+    return titlet;
 };
 
 // Do not edit below this line
